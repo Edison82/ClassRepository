15dd85ca62c00dd2305eaa152632f931fb9625f7
R andres <abolanos1994@gmail.com>
2025-06-18 18:13:36 -0500

    Commit payton by Rubio Andres

---
+++ actividad.py
@@ -0,0 +1,14 @@
+# Solicitar al usuario tres números
+num1 = float(input("Ingresa el primer número: "))
+num2 = float(input("Ingresa el segundo número: "))
+num3 = float(input("Ingresa el tercer número: "))
+
+# Sumar los tres números
+suma = num1 + num2 + num3
+
+# Calcular el promedio
+promedio = suma / 3
+
+# Mostrar los resultados
+print("La suma de los tres números es:", suma)
+print("El promedio de los tres números es:", promedio)
